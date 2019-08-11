dd8b5d52ca620107cb97874aa25d70ba4602ef70
Sebastian Skałacki <skalee@gmail.com>
2019-08-11 02:08:48 +0200

    Implement a trivial but functioning file picker

--- app/components/file-picker.js
+++ app/components/file-picker.js
@@ -1,7 +1,30 @@
 import Vue from "../../node_modules/vue/dist/vue.esm.browser.js"
 
 Vue.component("file-picker", {
+  methods: {
+    /// Clears the control's file list.
+    clearInput: function() {
+      this.$refs.pickerInput.files = new FileList
+    },
+
+    /// Adds given file to a current photo report.
+    emitFile: function(file) {
+      console.log(`Selected file: ${file.name}`)
+      this.report.addPicture(file)
+    },
+
+    /// Emits fileSelected event once per each file, then clears the input.
+    onFilesChange: function(event) {
+      for (var file of this.$refs.pickerInput.files) {
+        this.emitFile(file)
+      }
+      this.clearInput()
+    },
+  },
+
   template: `
-    <div>file-picker</div>
+    <div class="file-picker">
+      <input ref="pickerInput" type=file accept="image/png, image/jpeg" multiple v-on:change="onFilesChange"></input>
+    </div>
   `
 })
