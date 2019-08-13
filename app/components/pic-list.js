6db0c06622e2e874613358a71a6a600c9f0c9ba6
Sebastian Skałacki <skalee@gmail.com>
2019-08-13 17:06:11 +0200

    Display items in a pic-list component

--- app/components/pic-list.js
+++ app/components/pic-list.js
@@ -1,7 +1,17 @@
 import Vue from "../../node_modules/vue/dist/vue.esm.browser.js"
 
 Vue.component("pic-list", {
+  data: function() {
+    return {
+      pictures: this.report.pictures,
+    }
+  },
+
   template: `
-    <div>pic list</div>
+    <ol>
+      <li v-for="pic in pictures">
+        {{pic.originalFile.name}}
+      </li>
+    </ol>
   `
 })
