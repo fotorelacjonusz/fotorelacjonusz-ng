849b1f8747e520333dc0c32e9fcd4ff4bdd30d5c
Sebastian Skałacki <skalee@gmail.com>
2019-08-13 15:47:43 +0200

    Expose Report instance globally

--- app/gui.js
+++ app/gui.js
@@ -1,5 +1,9 @@
 import Vue from "../node_modules/vue/dist/vue.esm.browser.js"
+import { Report } from "./models/report.js"
 import "./components/main-window.js"
+
+// Expose Report instance across all the Vue components.
+Vue.prototype.report = new Report
 
 var app = new Vue({
   el: "#app",
