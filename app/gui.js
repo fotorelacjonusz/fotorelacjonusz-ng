7342a6c8c6d18101fef0f07cc0bf3c681e60f589
Sebastian Skałacki <skalee@gmail.com>
2019-09-20 03:51:44 +0200

    Use Vue-Router for navigating the application

--- app/gui.js
+++ app/gui.js
@@ -1,11 +1,22 @@
 import Vue from "../node_modules/vue/dist/vue.esm.browser.js"
+import VueRouter from "../node_modules/vue-router/dist/vue-router.esm.browser.js"
 import { Report } from "./models/report.js"
-import "./components/main-window.js"
+import { MainWindow } from "./components/main-window.js"
+
+Vue.use(VueRouter)
 
 // Expose Report instance across all the Vue components.
 Vue.prototype.report = new Report()
 
+const router = new VueRouter({
+  base: "/",
+  routes: [
+    { path: "/", component: MainWindow },
+  ]
+})
+
 var app = new Vue({
   el: "#app",
-  template: `<div id="app"><main-window></main-window></div>`
+  router,
+  template: `<div id="app"><router-view></router-view></div>`
 })
