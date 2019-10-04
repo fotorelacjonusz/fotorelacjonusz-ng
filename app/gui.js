35daf3c9b6099b601065a37bd2f3ed7b90e062ee
Sebastian Skałacki <skalee@gmail.com>
2019-10-04 11:01:13 +0200

    Add Font Awesome to the project dependencies

--- app/gui.js
+++ app/gui.js
@@ -1,11 +1,20 @@
 import Vue from "../node_modules/vue/dist/vue.esm.browser.js"
 import VueRouter from "../node_modules/vue-router/dist/vue-router.esm.browser.js"
+
+const { library } = require("@fortawesome/fontawesome-svg-core")
+const { fas } = require("@fortawesome/free-solid-svg-icons")
+const { FontAwesomeIcon } = require("@fortawesome/vue-fontawesome")
+
 import { Report } from "./models/report.js"
 import { AboutWindow } from "./components/about-window.js"
 import { ForumWindow } from "./components/forum-window.js"
 import { MainWindow } from "./components/main-window.js"
 
 Vue.use(VueRouter)
+
+// Font Awesome configuration
+library.add(fas) // whole bundle
+Vue.component("font-awesome-icon", FontAwesomeIcon)
 
 // Expose Report instance across all the Vue components.
 Vue.prototype.report = new Report()
