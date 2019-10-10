75baf9b45f28c251f73da69dd976af55db34d375
Sebastian Skałacki <skalee@gmail.com>
2019-10-10 15:16:29 +0200

    Add more picture management methods to Report model

--- app/models/report.js
+++ app/models/report.js
@@ -12,4 +12,56 @@ export class Report {
     this.pictures.push(pic)
     return pic
   }
+
+  deletePicture(pic) {
+    let all = this.pictures
+    let index = all.indexOf(pic)
+    if (index >= 0) {
+      all.splice(index, 1)
+    }
+  }
+
+  moveDown(pic) {
+    let all = this.pictures
+    let index = all.indexOf(pic)
+
+    if (index >= 0 && index < all.length - 1) {
+      swapItems(all, index, index + 1)
+    }
+  }
+
+  moveToBottom(pic) {
+    let all = this.pictures
+    let index = all.indexOf(pic)
+
+    if (index >= 0 && index < all.length - 1) {
+      swapItems(all, index, all.length - 1)
+    }
+  }
+
+  moveToTop(pic) {
+    let all = this.pictures
+    let index = all.indexOf(pic)
+
+    if (index >= 1) {
+      swapItems(all, index, 0)
+    }
+  }
+
+  moveUp(pic) {
+    let all = this.pictures
+    let index = all.indexOf(pic)
+
+    if (index >= 1) {
+      swapItems(all, index, index - 1)
+    }
+  }
+}
+
+// Due to Vue limitations, Array.splice is used.
+// See: https://vuejs.org/v2/guide/list.html#Caveats
+function swapItems(list, index1, index2) {
+  let swapped = list[index1]
+  list.splice(index1, 1)
+  list.splice(index2, 0, swapped)
 }
--- spec/models/report-spec.js
+++ spec/models/report-spec.js
@@ -33,4 +33,160 @@ describe("Report model", function() {
       expect(addedItem.originalFile).toBe(file)
     })
   })
+
+  describe(".deletePicture(file)", function() {
+    it("deletes given picture from the report", function() {
+      let [pic1, pic2, pic3, pic4, pic5] =
+        new Array(5).fill().map(() => factory.picture())
+
+      let report = factory.report([pic1, pic2, pic3, pic4, pic5])
+      report.deletePicture(pic2)
+      expect(report.pictures).toEqual([pic1, pic3, pic4, pic5])
+      report.deletePicture(pic5)
+      expect(report.pictures).toEqual([pic1, pic3, pic4])
+      report.deletePicture(pic1)
+      expect(report.pictures).toEqual([pic3, pic4])
+    })
+
+    it("does nothing if given picture does not belong to the report", function() {
+      let pic1 = factory.picture()
+      let pic2 = factory.picture()
+      let pic3 = factory.picture()
+      let report = factory.report([pic1, pic2])
+      report.deletePicture(pic3)
+      expect(report.pictures).toEqual([pic1, pic2])
+    })
+  })
+
+  describe(".moveDown(file)", function() {
+    it("swaps given picture with its next sibling in the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+
+      let report = factory.report([pic1, pic2, pic3])
+
+      report.moveDown(pic1)
+      expect(report.pictures).toEqual([pic2, pic1, pic3])
+      report.moveDown(pic1)
+      expect(report.pictures).toEqual([pic2, pic3, pic1])
+      report.moveDown(pic2)
+      expect(report.pictures).toEqual([pic3, pic2, pic1])
+    })
+
+    it("does nothing if given picture does not belong to the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      let other = factory.picture()
+      report.moveDown(other)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+
+    it("does nothing if given picture is already the last picture in the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      report.moveDown(pic3)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+  })
+
+  describe(".moveUp(file)", function() {
+    it("swaps given picture with its preceding sibling in the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+
+      let report = factory.report([pic1, pic2, pic3])
+
+      report.moveUp(pic3)
+      expect(report.pictures).toEqual([pic1, pic3, pic2])
+      report.moveUp(pic3)
+      expect(report.pictures).toEqual([pic3, pic1, pic2])
+      report.moveUp(pic1)
+      expect(report.pictures).toEqual([pic1, pic3, pic2])
+    })
+
+    it("does nothing if given picture does not belong to the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      let other = factory.picture()
+      report.moveUp(other)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+
+    it("does nothing if given picture is already the first picture in the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      report.moveUp(pic1)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+  })
+
+  describe(".moveToBottom(file)", function() {
+    it("moves given picture to the end of the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+
+      let report = factory.report([pic1, pic2, pic3])
+
+      report.moveToBottom(pic2)
+      expect(report.pictures).toEqual([pic1, pic3, pic2])
+      report.moveToBottom(pic1)
+      expect(report.pictures).toEqual([pic3, pic2, pic1])
+      report.moveToBottom(pic3)
+      expect(report.pictures).toEqual([pic2, pic1, pic3])
+    })
+
+    it("does nothing if given picture does not belong to the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      let other = factory.picture()
+      report.moveToBottom(other)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+
+    it("does nothing if given picture is already the last picture in the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      report.moveToBottom(pic3)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+  })
+
+  describe(".moveToTop(file)", function() {
+    it("moves given picture to the begining of the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+
+      let report = factory.report([pic1, pic2, pic3])
+
+      report.moveToTop(pic2)
+      expect(report.pictures).toEqual([pic2, pic1, pic3])
+      report.moveToTop(pic3)
+      expect(report.pictures).toEqual([pic3, pic2, pic1])
+      report.moveToTop(pic1)
+      expect(report.pictures).toEqual([pic1, pic3, pic2])
+    })
+
+    it("does nothing if given picture does not belong to the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      let other = factory.picture()
+      report.moveToTop(other)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+
+    it("does nothing if given picture is already the first picture in the report", function() {
+      let [pic1, pic2, pic3] =
+        new Array(3).fill().map(() => factory.picture())
+      let report = factory.report([pic1, pic2, pic3])
+      report.moveToTop(pic1)
+      expect(report.pictures).toEqual([pic1, pic2, pic3])
+    })
+  })
 })
