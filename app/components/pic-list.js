2a24ce5c3baa6801b21804cbf569d782ef8eb496
Sebastian Skałacki <skalee@gmail.com>
2019-10-10 15:27:05 +0200

    Add icon buttons to Pic List component

--- app/components/pic-list.js
+++ app/components/pic-list.js
@@ -9,7 +9,7 @@ export const PicList = {
 
   template: `
     <ol class="pic-list">
-      <li v-for="pic in pictures" class="box">
+      <li v-for="pic in pictures" class="box pic-item">
         <div class="level">
           <div class="level-item">
             <textarea
@@ -17,6 +17,34 @@ export const PicList = {
                 class="textarea has-fixed-size "
                 rows="2">
             </textarea>
+          </div>
+
+          <div class="level-right pic-list-icons">
+            <font-awesome-icon
+                icon="angle-up"
+                class="level-item"
+                title="Move up"
+                @click="report.moveUp(pic)" />
+            <font-awesome-icon
+                icon="angle-down"
+                class="level-item"
+                title="Move down"
+                @click="report.moveDown(pic)" />
+            <font-awesome-icon
+                icon="angle-double-up"
+                class="level-item"
+                title="Move to top"
+                @click="report.moveToTop(pic)" />
+            <font-awesome-icon
+                icon="angle-double-down"
+                class="level-item"
+                title="Move to bottom"
+                @click="report.moveToBottom(pic)" />
+            <font-awesome-icon
+                icon="trash"
+                class="level-item"
+                title="Delete from the report"
+                @click="report.deletePicture(pic)" />
           </div>
         </div>
 
