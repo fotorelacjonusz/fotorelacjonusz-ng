c0aa6538ee8a3d4ce3124d26c1e5d2467fd5a91b
Sebastian Skałacki <skalee@gmail.com>
2019-10-13 01:52:12 +0200

    Define a Countdown component
    Implement a simple countdown timer.

---
+++ app/components/countdown.js
@@ -0,0 +1,56 @@
+const Vue = require("vue/dist/vue.common.js")
+
+/// A minimalistic countdown timer.  Emits "zero" event when timer hits 0.
+export const Countdown = {
+  data: function() {
+    return {
+      seconds: 0,
+      timer: null,
+    }
+  },
+
+  template: `
+    <span class="countdown">{{displayValue}}</span>
+  `,
+
+  computed: {
+    displayValue: function() {
+      return this.timer ? `Waiting: ${this.seconds}s` : ""
+    }
+  },
+
+  methods: {
+    /// Sets timer to given number of seconds, and starts the countdown.
+    /// If there is a countdown in progress already, it is stopped without
+    /// emitting any events.
+    restart: function(seconds) {
+      stopTimer.call(this)
+      this.seconds = seconds
+      startTimer.call(this)
+    },
+  }
+}
+
+Vue.component("countdown", Countdown)
+
+// private
+
+function startTimer() {
+  this.timer = setInterval(() => tick.call(this), 1000)
+}
+
+function stopTimer() {
+  if (this.timer) {
+    clearInterval(this.timer)
+    this.timer = null
+  }
+}
+
+function tick() {
+  if (this.seconds > 0) {
+    this.seconds -= 1
+  } else {
+    this.$emit("zero")
+    stopTimer.call(this)
+  }
+}
