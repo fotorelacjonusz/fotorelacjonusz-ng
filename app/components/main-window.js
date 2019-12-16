c4188f55a36a4f32a5c33a100c586d28b5565526
Sebastian Skałacki <skalee@gmail.com>
2019-12-16 05:41:07 +0100

    Get rid of a separate Main Navbar component
    All the other window components have navigation buttons in their own
    template.  This appears to be actually less messy and more practical.

--- app/components/main-window.js
+++ app/components/main-window.js
@@ -1,12 +1,33 @@
 const Vue = require("vue/dist/vue.common.js")
 
-import "./main-navbar.js"
+import "./file-picker.js"
 import "./pic-list.js"
 
 export const MainWindow = {
   template: `
     <div class="main-window flex-window">
-      <main-navbar/>
+      <nav class="my-navbar" role="navigation">
+        <file-picker class="button is-primary"/>
+        <router-link
+            to="/forum"
+            class="button is-light"
+            v-translate>
+            Upload
+        </router-link>
+        <router-link
+            to="/settings"
+            class="button is-light"
+            v-translate>
+            Settings
+        </router-link>
+        <router-link
+            to="/about"
+            class="button is-light"
+            v-translate>
+            About this program
+        </router-link>
+      </nav>
+
       <pic-list class="spread"/>
     </div>
   `,
