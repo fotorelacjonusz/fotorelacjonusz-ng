06f4dbbf5c0abb33a8035be908e6f3627f3b8ebe
Sebastian Skałacki <skalee@gmail.com>
2019-12-18 04:53:26 +0100

    Render header and footer in BB Code Renderer

--- app/renderers/bb-code.js
+++ app/renderers/bb-code.js
@@ -13,10 +13,15 @@ export class BBCodeRenderer {
 
   toPosts() {
     let slices = this.report.sliced()
-    return slices.map((s) => this.renderPost(s.pictures, s.startIndex + 1))
+    return slices.map((s, idx) => {
+      let isFirstSlice = (idx === 0)
+      let isLastSlice = (idx === slices.length - 1)
+      return this.renderPost(s.pictures, s.startIndex + 1,
+        isFirstSlice, isLastSlice)
+    })
   }
 
-  renderPost(pics, startNum) {
+  renderPost(pics, startNum, isFirstSlice, isLastSlice) {
     let renderedPics = pics.map((pic, idx) => {
       return currentSettings.interpolatePostTemplate({
         "DESCRIPTION": pic.text.trim(),
@@ -24,6 +29,14 @@ export class BBCodeRenderer {
         "NUMBER": startNum + idx,
       })
     })
+
+    if (isFirstSlice) {
+      renderedPics.unshift(this.report.header.trim())
+    }
+
+    if (isLastSlice) {
+      renderedPics.push(this.report.footer.trim())
+    }
 
     let post = renderedPics.join("\n\n")
 
