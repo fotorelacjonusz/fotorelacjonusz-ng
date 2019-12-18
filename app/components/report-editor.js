5755c434cc452579455a122bf28df19867ae65fe
Sebastian Skałacki <skalee@gmail.com>
2019-12-18 04:42:07 +0100

    Add more textareas to Report Editor component
    Add fields for manipulating header and footer report attributes.

--- app/components/report-editor.js
+++ app/components/report-editor.js
@@ -5,7 +5,23 @@ import "./pic-list.js"
 export const ReportEditor = {
   template: `
     <div class="report-editor">
+      <p class="box">
+        <textarea
+            v-model="report.header"
+            class="textarea has-fixed-size "
+            rows="4">
+        </textarea>
+      </p>
+
       <pic-list />
+
+      <p class="box">
+        <textarea
+            v-model="report.footer"
+            class="textarea has-fixed-size "
+            rows="4">
+        </textarea>
+      </p>
     </div>
   `
 }
