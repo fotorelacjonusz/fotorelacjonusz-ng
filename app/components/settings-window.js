649fc99bc4471dc0ed8885dcec3e5581153e9052
Sebastian Skałacki <skalee@gmail.com>
2019-12-22 08:49:04 +0100

    Define interface for switching hosting service

--- app/components/settings-window.js
+++ app/components/settings-window.js
@@ -242,6 +242,30 @@ export const SettingsWindow = {
               </div>
           </div>
         </div>
+
+        <div class="settings-section">
+          <h3 class="title is-5" v-translate>Photo upload</h3>
+
+          <div class="field is-horizontal">
+            <div class="field-label is-normal">
+              <label class="label" v-translate>Hosting service</label>
+            </div>
+
+            <div class="field-body">
+              <div class="field">
+                <div class="control select">
+                  <select
+                      v-model="model.uploader.current"
+                      @change="onConfigUpdated">
+                      <option value="ImgurAnonUploader" v-translate>
+                          Imgur (anonymously)
+                      </option>
+                  </select>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   `,
