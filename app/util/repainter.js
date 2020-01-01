ec690c49858dd9e692111c98eb94e3f47ee349f8
Sebastian Skałacki <skalee@gmail.com>
2020-01-02 00:55:06 +0100

    Define .repaint() helper function
    Define a function which repaints (resizes, etc.) pictures.

---
+++ app/util/repainter.js
@@ -0,0 +1,37 @@
+const Jimp = require("jimp")
+
+import { currentSettings } from "../models/settings.js"
+
+export function repaint(jimpImage) {
+  return resize(jimpImage)
+}
+
+function getProcessingSettings() {
+  return currentSettings.data.processing
+}
+
+function resize(jimpImage) {
+  if (getProcessingSettings().resize.mode !== "normal") {
+    return jimpImage
+  }
+
+  const maxWidth = getProcessingSettings().resize.width
+  const maxHeight = getProcessingSettings().resize.height
+  const timesTooWide = jimpImage.bitmap.width / maxWidth
+  const timesTooHigh = jimpImage.bitmap.height / maxHeight
+
+  if (Math.max(timesTooWide, timesTooHigh) < 1) {
+    // Image is smaller than configured limits
+    return jimpImage
+  }
+
+  console.log("doing")
+
+  const requestedDimensions = (
+    timesTooWide > timesTooHigh
+    ? [maxWidth, Jimp.AUTO]
+    : [Jimp.AUTO, maxHeight]
+  )
+
+  return jimpImage.resize(...requestedDimensions)
+}
---
+++ spec/util/repainter-spec.js
@@ -0,0 +1,75 @@
+const Jimp = require("jimp")
+
+import { repaint } from "../../app/util/repainter.js"
+
+describe(".repaint()", function() {
+  beforeEach(async function() {
+    let flower192 = factory.file("flower-192.jpg")
+    this.img = await Jimp.read(flower192.path)
+    this.origImg = this.img.clone()
+  })
+
+  describe("when resizing is not enabled", function() {
+    beforeEach(function() {
+      stubCurrentSettings({processing: {resize: {mode: "never"}}})
+    })
+
+    it("keeps large images unchanged", async function() {
+      stubCurrentSettings({processing: {resize: {width: 100, height: 100}}})
+      let repainted = await repaint(this.img)
+      expect(repainted).toBeInstanceOf(Jimp)
+      expect(repainted.bitmap.width).toEqual(this.origImg.bitmap.width)
+      expect(repainted.bitmap.height).toEqual(this.origImg.bitmap.height)
+    })
+
+    it("keeps small images unchanged", async function() {
+      stubCurrentSettings({processing: {resize: {width: 1000, height: 1000}}})
+      let repainted = await repaint(this.img)
+      expect(repainted).toBeInstanceOf(Jimp)
+      expect(repainted.bitmap.width).toEqual(this.origImg.bitmap.width)
+      expect(repainted.bitmap.height).toEqual(this.origImg.bitmap.height)
+    })
+  })
+
+  describe("when resizing is enabled", function() {
+    beforeEach(function() {
+      stubCurrentSettings({processing: {resize: {mode: "normal"}}})
+    })
+
+    it("scales down image which is too wide", async function() {
+      stubCurrentSettings({processing: {resize: {width: 100, height: 1000}}})
+      let repainted = await repaint(this.img)
+      expect(repainted).toBeInstanceOf(Jimp)
+      expect(repainted.bitmap.width).toEqual(100)
+      expect(calculateRatio(repainted)).toBeCloseTo(calculateRatio(this.origImg))
+    })
+
+    it("scales down image which is too tall", async function() {
+      stubCurrentSettings({processing: {resize: {width: 1000, height: 100}}})
+      let repainted = await repaint(this.img)
+      expect(repainted).toBeInstanceOf(Jimp)
+      expect(repainted.bitmap.height).toEqual(100)
+      expect(calculateRatio(repainted)).toBeCloseTo(calculateRatio(this.origImg))
+    })
+
+    it("scales down image which is both too wide and too tall", async function() {
+      stubCurrentSettings({processing: {resize: {width: 100, height: 100}}})
+      let repainted = await repaint(this.img)
+      expect(repainted).toBeInstanceOf(Jimp)
+      expect(repainted.bitmap.width).toEqual(100)
+      expect(calculateRatio(repainted)).toBeCloseTo(calculateRatio(this.origImg))
+    })
+
+    it("keeps smaller images unchanged", async function() {
+      stubCurrentSettings({processing: {resize: {width: 1000, height: 1000}}})
+      let repainted = await repaint(this.img)
+      expect(repainted).toBeInstanceOf(Jimp)
+      expect(repainted.bitmap.width).toEqual(this.origImg.bitmap.width)
+      expect(repainted.bitmap.height).toEqual(this.origImg.bitmap.height)
+    })
+  })
+})
+
+function calculateRatio(jimpImage) {
+  return jimpImage.bitmap.width / jimpImage.bitmap.height
+}
