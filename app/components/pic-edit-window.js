251a8a896764720b6fcafd95accc7f02d1f0ca17
Sebastian Skałacki <skalee@gmail.com>
2020-01-06 01:07:20 +0100

    wip maybe

--- app/components/pic-edit-window.js
+++ app/components/pic-edit-window.js
@@ -1,12 +1,23 @@
 const Vue = require("vue/dist/vue.common.js")
 
+const { ImageEditor } = require("@toast-ui/vue-image-editor")
+
 export const PicEditWindow = {
+  components: {
+    "tui-image-editor": ImageEditor,
+  },
+
   data: function() {
     let index = parseInt(this.$route.params.num)
 
     return {
       index,
       picture: this.report.pictures[index],
+      useDefaultUI: true,
+      editorOptions: { // for tui-image-editor component's "options" prop
+          cssMaxWidth: 700,
+          cssMaxHeight: 500,
+      },
     }
   },
 
@@ -18,6 +29,9 @@ export const PicEditWindow = {
           title="Go back"
           @click="zoomOut" />
       </div>
+
+      <tui-image-editor :include-ui="useDefaultUI" :options="editorOptions">
+      </tui-image-editor>
 
       <img class="picture" :src="picture.displayUrl">
     </div>
