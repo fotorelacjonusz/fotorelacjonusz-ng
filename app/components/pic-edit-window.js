0deac742d74b9f3ef4e86fe8b2ccd6c9eff0b967
Sebastian Skałacki <skalee@gmail.com>
2020-01-06 01:54:04 +0100

    wip

--- app/components/pic-edit-window.js
+++ app/components/pic-edit-window.js
@@ -31,9 +31,12 @@ export const PicEditWindow = {
           @click="zoomOut" />
       </div>
 
-      <div ref="editor"></div>
+      <div ref="editor" style="height: 800px">
 
-      <img class="picture" :src="picture.displayUrl">
+          <canvas></canvas>
+          </div>
+
+      <!-- img class="picture" :src="picture.displayUrl" -->
     </div>
   `,
 
@@ -41,7 +44,27 @@ export const PicEditWindow = {
 
   methods: {
     setUpEditor() {
+
+      // var ImageEditor = require('tui-image-editor');
+      // var blackTheme = require('./js/theme/black-theme.js');
+// var locale_ru_RU = { // override default English locale to your custom
+//     'Crop': 'Обзрезать',
+//     'Delete-all': 'Удалить всё'
+//     // etc...
+// };
+
+
       this.instance = new ImageEditor(this.$refs.editor, {
+     includeUI: {
+         loadImage: {
+             url: this.picture.displayUrl,
+             name: 'SampleImage'
+         },
+         // locale: locale_ru_RU,
+         // theme: blackTheme, // or whiteTheme
+         initMenu: 'filter',
+         menuBarPosition: 'bottom'
+     },
         cssMaxWidth: 700,
         cssMaxHeight: 500,
         selectionStyle: {
