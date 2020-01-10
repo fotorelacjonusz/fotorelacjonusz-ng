603a2c746a5fe6a96c9a4b432f949a8b08943daa
Sebastian Skałacki <skalee@gmail.com>
2020-01-10 18:31:30 +0100

    Update indentation after recent changes

--- app/components/pic-list.js
+++ app/components/pic-list.js
@@ -13,58 +13,58 @@ export const PicList = {
         class="pic-list"
         :list="pictures"
         :disabled="!draggingAllowed">
-      <li v-for="(pic, idx) in pictures" class="box pic-item">
-        <div class="level">
-          <div class="level-left pic-list-number">
-              {{idx+1}}.
+        <li v-for="(pic, idx) in pictures" class="box pic-item">
+          <div class="level">
+            <div class="level-left pic-list-number">
+                {{idx+1}}.
+            </div>
+
+            <div class="level-item hide-on-view-mode-thumbs">
+              <textarea
+                  v-model="pic.text"
+                  class="textarea has-fixed-size "
+                  rows="2">
+              </textarea>
+            </div>
+
+            <div class="level-right pic-list-icons">
+              <font-awesome-icon
+                  icon="angle-up"
+                  class="level-item"
+                  title="Move up"
+                  @click="report.moveUp(pic)" />
+              <font-awesome-icon
+                  icon="angle-down"
+                  class="level-item"
+                  title="Move down"
+                  @click="report.moveDown(pic)" />
+              <font-awesome-icon
+                  icon="angle-double-up"
+                  class="level-item"
+                  title="Move to top"
+                  @click="report.moveToTop(pic)" />
+              <font-awesome-icon
+                  icon="angle-double-down"
+                  class="level-item"
+                  title="Move to bottom"
+                  @click="report.moveToBottom(pic)" />
+              <font-awesome-icon
+                  icon="trash"
+                  class="level-item"
+                  title="Delete from the report"
+                  @click="report.deletePicture(pic)" />
+            </div>
           </div>
 
-          <div class="level-item hide-on-view-mode-thumbs">
-            <textarea
-                v-model="pic.text"
-                class="textarea has-fixed-size "
-                rows="2">
-            </textarea>
-          </div>
+          <p><img :src="pic.displayUrl"></p>
+        </li>
 
-          <div class="level-right pic-list-icons">
-            <font-awesome-icon
-                icon="angle-up"
-                class="level-item"
-                title="Move up"
-                @click="report.moveUp(pic)" />
-            <font-awesome-icon
-                icon="angle-down"
-                class="level-item"
-                title="Move down"
-                @click="report.moveDown(pic)" />
-            <font-awesome-icon
-                icon="angle-double-up"
-                class="level-item"
-                title="Move to top"
-                @click="report.moveToTop(pic)" />
-            <font-awesome-icon
-                icon="angle-double-down"
-                class="level-item"
-                title="Move to bottom"
-                @click="report.moveToBottom(pic)" />
-            <font-awesome-icon
-                icon="trash"
-                class="level-item"
-                title="Delete from the report"
-                @click="report.deletePicture(pic)" />
-          </div>
-        </div>
-
-        <p><img :src="pic.displayUrl"></p>
-      </li>
-
-      <li
-          v-if="report.isEmpty"
-          class="box has-background-light has-text-weight-medium"
-          v-translate>
-          The report is empty.
-      </li>
+        <li
+            v-if="report.isEmpty"
+            class="box has-background-light has-text-weight-medium"
+            v-translate>
+            The report is empty.
+        </li>
     </draggable>
   `,
 
