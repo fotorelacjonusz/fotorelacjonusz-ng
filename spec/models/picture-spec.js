c09db8e99e04e38d1fd5d66af3f5528452f12dc4
Sebastian Skałacki <skalee@gmail.com>
2020-01-14 02:21:51 +0100

    Add some missing specs for Picture model

--- spec/models/picture-spec.js
+++ spec/models/picture-spec.js
@@ -2,12 +2,20 @@ import { Picture } from "../../app/models/picture.js"
 
 describe("Picture model", function() {
   it("is instantiable", function () {
-    expect(new Picture).toBeInstanceOf(Picture)
+    expect(new Picture(factory.file())).toBeInstanceOf(Picture)
   })
 
   it("has unique id", function() {
     let pic1 = new Picture
     let pic2 = new Picture
     expect(pic1.id).not.toEqual(pic2)
+  })
+
+  describe(".originalFile", function() {
+    it("stores a reference to a file passed to constructor", function() {
+      let file = factory.file()
+      let instance = new Picture(file)
+      expect(instance.originalFile).toBe(file)
+    })
   })
 })
