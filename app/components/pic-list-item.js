4079269a0c045e7d05d6b982f8bf953ee9c8f0bf
Sebastian Skałacki <skalee@gmail.com>
2020-01-14 03:44:32 +0100

    Fix indentation after recent changes

--- app/components/pic-list-item.js
+++ app/components/pic-list-item.js
@@ -9,56 +9,56 @@ export const PicListItem = {
   },
 
   template: `
-        <li class="box pic-item">
-          <div class="level pic-item-header">
-            <div class="level-left pic-list-number">
-                {{index+1}}.
-            </div>
+    <li class="box pic-item">
+      <div class="level pic-item-header">
+        <div class="level-left pic-list-number">
+            {{index+1}}.
+        </div>
 
-            <div class="level-item hide-on-view-mode-thumbs">
-              <textarea
-                  v-model="picture.text"
-                  class="textarea has-fixed-size "
-                  rows="2">
-              </textarea>
-            </div>
+        <div class="level-item hide-on-view-mode-thumbs">
+          <textarea
+              v-model="picture.text"
+              class="textarea has-fixed-size "
+              rows="2">
+          </textarea>
+        </div>
 
-            <div class="level-right pic-list-icons">
-              <font-awesome-icon
-                  icon="search-plus"
-                  class="level-item hide-on-view-mode-large"
-                  title="Move up"
-                  @click="zoomTo($event)" />
-              <font-awesome-icon
-                  icon="angle-up"
-                  class="level-item hide-on-view-mode-thumbs"
-                  title="Move up"
-                  @click="report.moveUp(picture)" />
-              <font-awesome-icon
-                  icon="angle-down"
-                  class="level-item hide-on-view-mode-thumbs"
-                  title="Move down"
-                  @click="report.moveDown(picture)" />
-              <font-awesome-icon
-                  icon="angle-double-up"
-                  class="level-item"
-                  title="Move to top"
-                  @click="report.moveToTop(picture)" />
-              <font-awesome-icon
-                  icon="angle-double-down"
-                  class="level-item"
-                  title="Move to bottom"
-                  @click="report.moveToBottom(picture)" />
-              <font-awesome-icon
-                  icon="trash"
-                  class="level-item"
-                  title="Delete from the report"
-                  @click="report.deletePicture(picture)" />
-            </div>
-          </div>
+        <div class="level-right pic-list-icons">
+          <font-awesome-icon
+              icon="search-plus"
+              class="level-item hide-on-view-mode-large"
+              title="Move up"
+              @click="zoomTo($event)" />
+          <font-awesome-icon
+              icon="angle-up"
+              class="level-item hide-on-view-mode-thumbs"
+              title="Move up"
+              @click="report.moveUp(picture)" />
+          <font-awesome-icon
+              icon="angle-down"
+              class="level-item hide-on-view-mode-thumbs"
+              title="Move down"
+              @click="report.moveDown(picture)" />
+          <font-awesome-icon
+              icon="angle-double-up"
+              class="level-item"
+              title="Move to top"
+              @click="report.moveToTop(picture)" />
+          <font-awesome-icon
+              icon="angle-double-down"
+              class="level-item"
+              title="Move to bottom"
+              @click="report.moveToBottom(picture)" />
+          <font-awesome-icon
+              icon="trash"
+              class="level-item"
+              title="Delete from the report"
+              @click="report.deletePicture(picture)" />
+        </div>
+      </div>
 
-          <p class="pic-item-main"><img :src="picture.displayUrl"></p>
-        </li>
+      <p class="pic-item-main"><img :src="picture.displayUrl"></p>
+    </li>
   `,
 
   methods: {
