0c1042e541f8f7ca38fbc6cd7e744dd2bd75a5df
Sebastian Skałacki <skalee@gmail.com>
2020-01-14 03:24:01 +0100

    Use longer variable names in Pic List component
    Rename variables after prop names which will be soon defined.

--- app/components/pic-list.js
+++ app/components/pic-list.js
@@ -13,15 +13,15 @@ export const PicList = {
         class="pic-list"
         :list="pictures"
         :disabled="!draggingAllowed">
-        <li v-for="(pic, idx) in pictures" :key="pic.id" class="box pic-item">
+        <li v-for="(picture, index) in pictures" :key="picture.id" class="box pic-item">
           <div class="level pic-item-header">
             <div class="level-left pic-list-number">
-                {{idx+1}}.
+                {{index+1}}.
             </div>
 
             <div class="level-item hide-on-view-mode-thumbs">
               <textarea
-                  v-model="pic.text"
+                  v-model="picture.text"
                   class="textarea has-fixed-size "
                   rows="2">
               </textarea>
@@ -37,31 +37,31 @@ export const PicList = {
                   icon="angle-up"
                   class="level-item hide-on-view-mode-thumbs"
                   title="Move up"
-                  @click="report.moveUp(pic)" />
+                  @click="report.moveUp(picture)" />
               <font-awesome-icon
                   icon="angle-down"
                   class="level-item hide-on-view-mode-thumbs"
                   title="Move down"
-                  @click="report.moveDown(pic)" />
+                  @click="report.moveDown(picture)" />
               <font-awesome-icon
                   icon="angle-double-up"
                   class="level-item"
                   title="Move to top"
-                  @click="report.moveToTop(pic)" />
+                  @click="report.moveToTop(picture)" />
               <font-awesome-icon
                   icon="angle-double-down"
                   class="level-item"
                   title="Move to bottom"
-                  @click="report.moveToBottom(pic)" />
+                  @click="report.moveToBottom(picture)" />
               <font-awesome-icon
                   icon="trash"
                   class="level-item"
                   title="Delete from the report"
-                  @click="report.deletePicture(pic)" />
+                  @click="report.deletePicture(picture)" />
             </div>
           </div>
 
-          <p class="pic-item-main"><img :src="pic.displayUrl"></p>
+          <p class="pic-item-main"><img :src="picture.displayUrl"></p>
         </li>
 
         <li
