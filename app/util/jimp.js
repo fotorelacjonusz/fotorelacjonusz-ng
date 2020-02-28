9e95e1b336ed3f7687b4171c8861a771fc5d226c
Sebastian Skałacki <skalee@gmail.com>
2020-02-28 08:19:03 +0100

    Cherry-pick Jimp plugins
    The only plugin we need now is one for resizing.
    
    This change should significantly reduce bundle size, and load time.

--- app/util/jimp.js
+++ app/util/jimp.js
@@ -1,8 +1,9 @@
 const configure = require("@jimp/custom")
 const types = require("@jimp/types")
-const plugins = require("@jimp/plugins")
 
 export const Jimp = configure({
   types: [types],
-  plugins: [plugins],
+  plugins: [
+    require("@jimp/plugin-resize"),
+  ],
 })
