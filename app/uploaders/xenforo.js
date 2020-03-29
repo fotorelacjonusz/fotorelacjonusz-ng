4251631caabfbc3d0ae91fddb624291607570a5f
Sebastian Skałacki <skalee@gmail.com>
2020-03-29 12:23:48 +0200

    some wip xenforo upl

---
+++ app/uploaders/xenforo.js
@@ -0,0 +1,53 @@
+const needle = require("needle")
+
+const IMGUR_BASE_URL = "https://api.imgur.com/3"
+
+export class ImgurAnonUploader {
+  async uploadFile(fileName, buffer) {
+    let form = this.buildUploadForm(fileName, buffer)
+    var response
+    try {
+      console.log(`Uploading "${fileName}" via ${this.constructor.name}`)
+      response = await this.makeUploadRequest(form)
+    } catch (error) {
+      console.error(error)
+      return null
+    }
+    let parsedResponse = JSON.parse(response.body)
+    if (!parsedResponse.success) {
+      console.error(`Upload went wrong.  Actual response was: ${response.body}`)
+      return null
+    }
+    console.log(`Uploaded successfully as ${parsedResponse.data.id}.`)
+
+    return {remoteUrl: parsedResponse.data.link, upload: parsedResponse.data}
+  }
+
+  /// In a format understandable to Needle.
+  /// https://www.npmjs.com/package/needle#multipart-post-passing-data-buffer
+  buildUploadForm(fileName, buffer) {
+    return {
+      type: "file",
+      image: {
+        buffer,
+        filename: fileName,
+        content_type: "application/octet-stream",
+      },
+    }
+  }
+
+  makeUploadRequest(form) {
+    return needle(
+      "post",
+      `${IMGUR_BASE_URL}/image`,
+      form,
+      {
+        headers: {
+          authorization: `Client-ID ${IMGUR_CLIENT_ID}`,
+        },
+        multipart: true,
+        parse_response: false, // I prefer to parse it manually.
+      },
+    )
+  }
+}
