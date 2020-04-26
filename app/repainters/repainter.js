ce20af740ae0b3b5b76e69705d8d0a7628ce70e1
Sebastian Skałacki <skalee@gmail.com>
2020-04-27 01:07:51 +0200

    Delete ExifRotater repainter
    EXIF orientation is correctly supported in Jimp since version 0.10.2,
    hence EXIF Rotater which responsible for that can be removed.  Tests
    for EXIF orientation handling are preserved, but moved so that they
    describe repainter as whole, not just EXIF Rotater.

--- app/repainters/repainter.js
+++ app/repainters/repainter.js
@@ -1,10 +1,8 @@
-import { autorotate } from "./exif-rotater.js"
 import { frame } from "./framer.js"
 import { resize } from "./resizer.js"
 import { watermark } from "./watermarker.js"
 
 export async function repaint(jimpImage) {
-  jimpImage = await autorotate(jimpImage)
   jimpImage = await resize(jimpImage)
   jimpImage = await watermark(jimpImage)
   jimpImage = await frame(jimpImage)
--- spec/repainters/repainter-spec.js
+++ spec/repainters/repainter-spec.js
@@ -29,4 +29,28 @@ describe(".repaint()", function() {
 
     expect(Resizer.resize.calledBefore(Watermarker.watermark)).toBeTruthy()
   })
+
+  for (let orientation = 1; orientation <= 8; orientation++) {
+    describe(`for picture with EXIF orientation = ${orientation}`, function() {
+      beforeEach(async function() {
+        this.normalImg = await imageWithOrientation(1)
+      })
+
+      beforeEach(async function() {
+        this.img = await imageWithOrientation(orientation)
+      })
+
+      it("rotates bitmap according to EXIF data", async function() {
+        let processed = await repaint(this.img)
+        expect(processed.getWidth()).toEqual(this.normalImg.getWidth())
+        expect(processed.getHeight()).toEqual(this.normalImg.getHeight())
+        expect(Jimp.distance(this.normalImg, processed)).toBeLessThan(0.05)
+      })
+    })
+  }
 })
+
+function imageWithOrientation(orientation) {
+  let path = factory.file(`lady-orientation-${orientation}.jpg`).path
+  return Jimp.read(path)
+}
